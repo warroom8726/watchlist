72a94f17274855dd07d156be633a7a04afe9fd23
chen wj <872664238@qq.com>
2022-01-06 14:27:49 +0800

    0106 add url_for <name>

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
 from flask import Flask
+from flask import url_for
+from werkzeug.utils import escape
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return 'hello watchlist!'
 
-@app.route('/abc')
+@app.route('/cn')
 def hello2():
-    return 'nihao watchlist!22'
+    return u'你好 watchlist!'
+
+@app.route('/gif')
+def hello_html():
+    return '<h1>hello totoro!</h1><img src="http://helloflask.com/totoro.gif">'
+
+@app.route('/user-<name>')
+def hello4(name):
+    return 'user is: %s' % escape(name)
+
+@app.route('/test')
+def hello_test():
+    print(url_for('hello_html'))
+    print('url-is:%s' % escape(('hello4')))
+    return '<h2>testfdddd督导费</h2>'
